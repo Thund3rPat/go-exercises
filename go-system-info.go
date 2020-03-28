3ff7a687933a27c39ec6001f32d2de95caef5e8b
Thund3rPat <patricksimuttis@gmail.com>
2020-03-28 21:21:06 +0100

    code written

--- go-system-info.go
+++ go-system-info.go
@@ -1,1 +1,21 @@
+package main
 
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
+
+func main() {
+	fmt.Println("-----go-system-info-----")
+
+	host, _ := os.Hostname()
+	fmt.Println("Hostname:", host)
+	fmt.Println("OS:", runtime.GOOS)
+	fmt.Println("Arch:", runtime.GOARCH)
+
+	path, _ := os.LookupEnv("GOPATH")
+	fmt.Println("GOPATH:", path)
+
+	fmt.Println("Go version:", runtime.Version())
+}
